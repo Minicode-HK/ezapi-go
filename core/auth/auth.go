e0663c6817e36afd3cfd15ce8f870bad8adffb4d
ken.lee <kpleeae@connect.ust.hk>
2025-12-05 13:16:14 +0800

    [feature] add support for wildcard pattern matching

--- core/auth/auth.go
+++ core/auth/auth.go
@@ -19,6 +19,7 @@ var GlobalAuthProvider Provider
 
 // you can specify excluded paths that do not require authentication
 func SetupAuthProvider(router *gin.Engine, provider Provider, excludedPaths ...string) {
+	excludedPaths = append(excludedPaths, "/login") 
 	router.Use(func(c *gin.Context) {
 		// Check if the current path is in the excluded paths
 		for _, path := range excludedPaths {
@@ -27,10 +28,19 @@ func SetupAuthProvider(router *gin.Engine, provider Provider, excludedPaths ...s
 				return
 			}
 
-			// pattern matching 
+			// pattern matching like /module/:id
 			if c.FullPath() == path {
 				c.Next()
 				return
+			}
+
+			// wildcard matching like /module/*
+			if len(path) > 0 && path[len(path) - 1] == '*' {
+				prefix := path[:len(path)-1]
+				if len(c.Request.URL.Path) > len(prefix) && prefix == c.Request.URL.Path[:len(path)-1] {
+					c.Next()
+					return
+				}
 			}
 		}
 
