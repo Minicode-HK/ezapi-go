7921d83272f0b049876089818a49134a57952eea
Ryuuu825 <kpleeae@connect.ust.hk>
2026-05-12 17:23:50 +0800

    [feature] standlized http response

---
+++ ez/HttpResponse.go
@@ -0,0 +1,40 @@
+package ez
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// Below is some helper function to standardize http responses with gin
+
+func SendSuccess(c *gin.Context, data interface{}, extra ...interface{}) {
+	response := gin.H{
+		"success": true,
+		"data":    data,
+	}
+
+	// If extra data is provided, merge it into the response
+	if len(extra) > 0 {
+		if extraInfo, ok := extra[0].(gin.H); ok {
+			for key, value := range extraInfo {
+				response[key] = value
+			}
+		}
+	}
+
+	c.JSON(200, response)
+}
+
+func SendError(c *gin.Context, code int, message string) {
+	c.JSON(code, gin.H{
+		"success": false,
+		"message": message,
+	})
+}
+
+func SendErrorWithDetails(c *gin.Context, code int, message string, details interface{}) {
+	c.JSON(code, gin.H{
+		"success": false,
+		"message": message,
+		"details": details,
+	})
+}
